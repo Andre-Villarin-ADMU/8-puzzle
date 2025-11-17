1181ceada9f8df924c635560088e28c1f6cbd56a
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-11-18 00:11:09 +0800

    Added working BFS implementation and cleaned up code.

--- 8puzzle_bfs.py
+++ 8puzzle_bfs.py
@@ -1,9 +1,12 @@
 import random
 import math
+from collections import deque
+import time
 
 rows, cols = (3,3)
-ELEMENTS = [" ", 1, 2, 3, 4, 5, 6, 7, 8]
-GOAL_STATE = [1, 2, 3, 8, " ", 4, 7, 6, 5]
+BLANK = " "
+ELEMENTS = [BLANK, 1, 2, 3, 4, 5, 6, 7, 8]
+GOAL_STATE = (1, 2, 3, 4, 5, 6, 7, 8, BLANK)
 HEADER_1 = "8 Puzzle - Breadth First Search (BFS) Algorithm"
 BORDER = "=" * len(HEADER_1)
 HEADER = [BORDER, HEADER_1]
@@ -15,23 +18,23 @@ MOVE_OFFSETS = {
     "right": 1
 }
 
-def random_selector(elements, num_elements_to_select) -> list:
+def random_selector(elements, num_elements_to_select) -> tuple:
     '''
     Randomly chooses from ELEMENTS
     and determines the order in which
-    these are added to a list raw_arr.
+    these are added to a tuple state.
     '''
-    raw_arr = []
+    state = []
 
     while (num_elements_to_select > 0):
         selected = random.choice(elements)
         elements.pop(elements.index(selected))
-        raw_arr.append(selected)
+        state.append(selected)
         num_elements_to_select -= 1
 
-    return raw_arr
+    return tuple(state)
 
-def arr_builder(raw_arr) -> list:
+def arr_builder(raw_arr: list) -> list:
     '''
     Takes a 1D list 'raw_arr' and builds
     a nested list to create the
@@ -45,13 +48,15 @@ def arr_builder(raw_arr) -> list:
 
     return built_arr
 
-def ui_helper(my_arr: list) -> str:
+def ui_helper(my_arr: list, is_BFS: bool, iter_number: int) -> str:
     '''
     This helper method references a list
     object 'my_arr' for printing out a minimal
     GUI in the console.
     '''
-    str_arr = f"{HEADER[0]}\n{HEADER[1]}\n{HEADER[0]}\n\n"
+    if is_BFS: count_label = "(BFS) Depth #: "
+    else: count_label = "(Non-BFS) Turn #: " 
+    str_arr = f"{HEADER[0]}\n{HEADER[1]}\n{count_label}{iter_number}\n{HEADER[0]}\n\n"
 
     for i in range(len(my_arr)):
         for j in range (len(my_arr[i])):
@@ -60,67 +65,125 @@ def ui_helper(my_arr: list) -> str:
         
     return str_arr
 
-def is_blank(my_arr: list, index_to_move: int, move: str):
-    offset = MOVE_OFFSETS[move]
-    to_check = my_arr[index_to_move + offset]
-    return to_check == " "
-
-def get_moves(my_arr, index_to_move: int) -> list:
-    row = index_to_move // 3
-    col = index_to_move % 3
-    available_moves = []
-    valid_moves = []
-    if (row > 0): 
-        available_moves.append("up")
-    if (row < 2): 
-        available_moves.append("down")
-    if (col > 0): 
-        available_moves.append("left")
-    if (col < 2): 
-        available_moves.append("right")
-
-    for move in available_moves:
-        print(move)
-        if is_blank(my_arr, index_to_move, move):
-            valid_moves.append(move)
-
-    return valid_moves
-
-def is_goal_state(my_arr) -> bool:
-    return GOAL_STATE == my_arr
-def move_tile(my_arr: list, tile):
+def get_moves(index_to_move: int) -> list:
     try:
-        index = my_arr.index(int(tile))
-        valid_moves = get_moves(my_arr, index)
+        row = index_to_move // 3
+        col = index_to_move % 3
+        valid_moves = []
+        if (row > 0): 
+            valid_moves.append("up")
+        if (row < 2): 
+            valid_moves.append("down")
+        if (col > 0): 
+            valid_moves.append("left")
+        if (col < 2): 
+            valid_moves.append("right")
 
-        for move in valid_moves:
-            offset = MOVE_OFFSETS[move]
-            my_arr[index + offset] = my_arr[index]
-            my_arr[index] = " "
-
+        return valid_moves
+    
     except ValueError:
         pass
 
+def is_goal_state(my_arr: tuple) -> bool:
+    return GOAL_STATE == my_arr
 
-# Some test code:
-unformatted_list = random_selector(ELEMENTS, NUM_ELEMENTS_TO_SELECT)
-formatted_list = arr_builder(unformatted_list)
-print(ui_helper(formatted_list))
+def move_tile(my_arr: list, valid_moves, selected_move: str) -> list:
+    new_arr = my_arr.copy()
+    try:
+        offset = MOVE_OFFSETS[selected_move]
+        new_arr[index] = new_arr[index + offset]
+        new_arr[index + offset] = BLANK
 
-is_manual = True
-mode = input(("Manual Mode (non-BFS)? (Enter Y/N): "))
-if mode == "Y": is_manual
-else: is_manual = False
-if is_manual:
-    num_turns = 0
-    while not is_goal_state(unformatted_list):
-        tile = input("What tile to move (enter a digit): ")
-        move_tile(unformatted_list, tile)
+    except:
+        pass
+
+    return new_arr
+
+def find_neighbors(current_state: tuple) -> tuple:
+    index = current_state.index(BLANK)
+    valid_moves = get_moves(index)
+    neighbor_result = []
+    for move in valid_moves:
+        new_state = list(current_state)
+        new_state[index] = new_state[index + MOVE_OFFSETS[move]]
+        new_state[index + MOVE_OFFSETS[move]] = BLANK
+        neighbor_result.append((tuple(new_state), move))
+    
+    return neighbor_result
+
+def get_path(parent: dict, moves: dict, goal_state: tuple) -> list:
+    path = []
+    move_arr = []
+    state = goal_state
+
+    while state is not None:
+        path.append(state)
+        move_arr.append(moves[state])
+        state = parent[state]
+    
+    path.reverse()
+    move_arr.reverse()
+
+    return path, move_arr
+
+def print_solution(path, move_arr):
+    for i, state in enumerate(path):
+        if i == 0:
+            print("Start: ")
+        else:
+            print(f"Move {i}: {move_arr[i]}")
+        current_state = arr_builder(list(state))
+        print(ui_helper(current_state, False, i))
+
+def bfs(init_state: tuple):
+    root_node = init_state
+    parent = {root_node: None}
+    moves = {root_node: None}
+    visited = {root_node: True}
+    bfs_queue = deque([root_node])
+    iter = 0
+
+    while len(bfs_queue) > 0:
+        current_state = bfs_queue.popleft()
+
+        if is_goal_state(current_state):
+            print(f"Solution found at iteration {iter+1}!")
+            path, move_arr = get_path(parent, moves, current_state)
+            print_solution(path, move_arr)
+            return path, move_arr
+        
+        for new_state, move in find_neighbors(current_state):
+            if new_state not in visited:
+                visited[new_state] = True
+                moves[new_state] = move
+                parent[new_state] = current_state
+                bfs_queue.append(new_state)
+        iter += 1
+    
+    print("No solution exists. Start state may be of odd parity.")
+    return None
+
+
+# will clean up the code
+is_BFS = True
+state = random_selector(ELEMENTS, NUM_ELEMENTS_TO_SELECT)
+formatted_list = arr_builder(list(state))
+print(ui_helper(formatted_list, is_BFS, 0))
+
+mode = input(("Use BFS? (Y/N): "))
+if mode == "N": is_BFS = False
+
+if is_BFS:
+    bfs(state)
+else:
+    num_turns = 1
+    while not is_goal_state(state):
+        index = state.index(BLANK)
+        valid_moves = get_moves(index)
+        move = input(f"Move blank to? ({valid_moves}): ")
+        unformatted_list = move_tile(list(state), valid_moves, move)
         formatted_list = arr_builder(unformatted_list)
-        print(ui_helper(formatted_list))
+        state = tuple(unformatted_list)
+        print(ui_helper(formatted_list, is_BFS, num_turns))
         num_turns += 1
     print(f"Completed in {num_turns} moves")
-
-else:
-    # bfs implementation
-    pass

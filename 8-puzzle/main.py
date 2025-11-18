e286b3156bd283b0d4d6e83774b1e60f994d20ff
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-11-18 14:46:15 +0800

    Organized py files into a directory

---
+++ 8-puzzle/main.py
@@ -0,0 +1,30 @@
+from bfs import *
+
+def main():
+    is_BFS = True
+    test_state = (8, 6, 7, 2, 5, 4, 3, BLANK, 1) # most difficult but solvable state
+    state = test_state
+    # state = random_selector(ELEMENTS, NUM_ELEMENTS_TO_SELECT)
+    formatted_list = arr_builder(list(state))
+    print(ui_helper(formatted_list, is_BFS, 0))
+
+    mode = input(("Use BFS? (Y/N): "))
+    if mode == "N": is_BFS = False
+
+    if is_BFS:
+        bfs(state)
+    else:
+        num_turns = 1
+        while not is_goal_state(state):
+            index = state.index(BLANK)
+            valid_moves = get_moves(index)
+            move = input(f"Move blank to? ({valid_moves}): ")
+            unformatted_list = move_tile(list(state), valid_moves, move)
+            formatted_list = arr_builder(unformatted_list)
+            state = tuple(unformatted_list)
+            print(ui_helper(formatted_list, is_BFS, num_turns))
+            num_turns += 1
+        print(f"Completed in {num_turns} moves")
+
+if __name__ == "__main__":
+    main()
